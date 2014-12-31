45dfa8bb70cc7ff766d1579e94b15f403ebcf7a8
Steven John Miller <steven.jmiller@gmail.com>
2014-12-31 14:34:21 +1100

    Need to change setbase to node equivalent

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 var EmailBuilder = require('./');
+var fs = require('fs');
 
 var emailBuilder = new EmailBuilder();
 
-emailBuilder.run('./test/example/html/htmlTest/html', './test/example/result/');
+fs.readFile('test/example/html/htmlTest.html', 'utf8', function (err, data) {
+  if (err) throw err;
+  emailBuilder.run(data, './test/example/result/');
+});
